08c74b659ed1503d252736f5de0cad60a6ea7357
renzothemartian <smelter03@gmail.com>
2021-06-19 17:16:39 -0600

    oo

--- scripts/servo.py
+++ scripts/servo.py
@@ -20,16 +20,18 @@ p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
 p.start(2.5)  # Initialization
 try:
   while True:
+    print("start")
     p.ChangeDutyCycle(5)
     time.sleep(0.5)
-    print("5")
     p.ChangeDutyCycle(7.5)
     time.sleep(0.5)
-    print("7.5")
     p.ChangeDutyCycle(10)
     time.sleep(0.5)
     p.ChangeDutyCycle(12.5)
-    time.sleep(3)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(15)
+    print('15')
+    time.sleep(10)
     p.ChangeDutyCycle(10)
     time.sleep(0.5)
     p.ChangeDutyCycle(7.5)
