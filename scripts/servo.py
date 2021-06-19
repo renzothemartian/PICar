aa2cd87fcb24cfd50e28d5cf4789e4de1e79fea4
renzothemartian <smelter03@gmail.com>
2021-06-19 17:32:31 -0600

    oo

--- scripts/servo.py
+++ scripts/servo.py
@@ -16,27 +16,34 @@ servoPIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 100)  # GPIO 17 for PWM with 50Hz
-p.start(2.5)  # Initialization
-try:
-  while True:
-    print("start")
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
+p = GPIO.PWM(servoPIN, 0.5)  # GPIO 17 for PWM with 50Hz
+# p.start(2.5)  # Initialization
+# try:
+#   while True:
+#     print("start")
+#     p.ChangeDutyCycle(5)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(7.5)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(10)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(12.5)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(10)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(7.5)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(5)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(2.5)
+#     time.sleep(0.5)
+# except KeyboardInterrupt:
+#   p.stop()
+#   GPIO.cleanup()
+
+
+# p = GPIO.PWM(12, 0.5)
+p.start(1)
+input('Press return to stop:')   # use raw_input for Python 2
+p.stop()
+GPIO.cleanup()
