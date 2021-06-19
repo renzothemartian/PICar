f0e09c38a27ea2f8e07a03eed69412c1823b7700
ctaggart27 <ctaggart27@gmail.com>
2021-06-19 12:25:12 -0600

    Christian is commiting something on git

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # PICar
 
 pi@192.168.1.299
+
+
+## dependencies
+`sudo apt-get install python3-dev python3-rpi.gpio`
--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,18 @@
 import sys;
 
-def biglogs():
-    print("im logging a log")
+# def biglogs():
+#     print("im logging a log")
+
+# if __name__ == '__main__':
+#     globals()[sys.argv[1]]()
+
+# Take input from cli; pass as arg
+def input(mystring):
+    print(mystring)
+
 
 if __name__ == '__main__':
-    globals()[sys.argv[1]]()
+    if sys.argv[1] == 'biglogs':
+        globals()[sys.argv[1]]()
+    else:
+        globals()[sys.argv[1]](sys.argv[2])
