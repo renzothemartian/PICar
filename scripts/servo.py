9772ded98729e2546f92ca52e67039776e64bb5f
renzothemartian <smelter03@gmail.com>
2021-06-19 17:40:46 -0600

    oo

--- scripts/servo.py
+++ scripts/servo.py
@@ -38,7 +38,7 @@ try:
     # time.sleep(0.5)
     # p.ChangeDutyCycle(2.5)
     # time.sleep(0.5)
-    # p.start(1)
+    p.start(1)
     # input('Press return to stop:')   # use raw_input for Python 2
     # p.stop()
     # GPIO.cleanup()
