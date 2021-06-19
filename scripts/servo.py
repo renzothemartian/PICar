f747a898c771ac703493231b8e1cb7e972bb186b
renzothemartian <smelter03@gmail.com>
2021-06-19 17:35:52 -0600

    oo

--- scripts/servo.py
+++ scripts/servo.py
@@ -37,10 +37,10 @@ try:
     # time.sleep(0.5)
     # p.ChangeDutyCycle(2.5)
     # time.sleep(0.5)
-    p.start(1)
-    input('Press return to stop:')   # use raw_input for Python 2
-    p.stop()
-    GPIO.cleanup()
+    # p.start(1)
+    # input('Press return to stop:')   # use raw_input for Python 2
+    # p.stop()
+    # GPIO.cleanup()
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
