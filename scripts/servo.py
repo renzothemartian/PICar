68a57a28ee715a3fdd03fa7e112b0a57efeb1675
renzothemartian <smelter03@gmail.com>
2021-06-19 17:33:26 -0600

    oo

--- scripts/servo.py
+++ scripts/servo.py
@@ -43,7 +43,7 @@ p = GPIO.PWM(servoPIN, 0.5)  # GPIO 17 for PWM with 50Hz
 
 
 # p = GPIO.PWM(12, 0.5)
-p.start(1)
+p.start(2.5)
 input('Press return to stop:')   # use raw_input for Python 2
 p.stop()
 GPIO.cleanup()
