ab7246e91224c73035071ac8b6ef591ee07c8893
renzothemartian <smelter03@gmail.com>
2021-06-19 21:04:57 -0600

    0

--- scripts/arduconnect.py
+++ scripts/arduconnect.py
@@ -1,5 +1,5 @@
 import os
 
-cmd = "screen usb-1a86_USB_Serial-if00-port0 9600"
+cmd = "screen /dev/serial/by-id/usb-1a86_USB_Serial-if00-port0 9600"
 
 returned_value = os.system(cmd)  # returns the exit code in unix
