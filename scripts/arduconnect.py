3b35aa194269819a92cce07d43b08e6ed2a55f1a
renzothemartian <smelter03@gmail.com>
2021-06-19 21:18:08 -0600

    changed to ardu

--- scripts/arduconnect.py
+++ scripts/arduconnect.py
@@ -1,5 +1,5 @@
 import os
 
-cmd = "screen /dev/serial/by-id/usb-1a86_USB_Serial-if00-port0 9600"
+cmd = "sudo screen /dev/serial/by-id/usb-1a86_USB_Serial-if00-port0 9600"
 
 returned_value = os.system(cmd)  # returns the exit code in unix
