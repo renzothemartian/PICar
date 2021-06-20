d1dc58215e2bdc6fd1b30bc5d63c8293f9892ca0
renzothemartian <smelter03@gmail.com>
2021-06-19 21:23:39 -0600

    0

--- scripts/ardu.py
+++ scripts/ardu.py
@@ -1,10 +1,10 @@
 import serial
 #init serial port and bound
 # bound rate on two ports must be the same
-ser = serial.Serial('/dev/ttyACM1', 9600)
+ser = serial.Serial('/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0', 9600)
 print(ser.portstr)
 
 #send data via serial port
-serialcmd=("012345688902341")
+serialcmd=("90")
 ser.write(serialcmd.encode())
 ser.close()
